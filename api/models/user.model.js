0f3c06d76d6465322e1cecd3d643529bf372087b
mahindraD <d.mahindra04010611@gmail.com>
2024-05-23 00:02:13 +0530

    create user modal

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+    username : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password : {
+        type: String,
+        required: true,
+    },
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
