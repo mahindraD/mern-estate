32a027aa8254ad9b871b479d9d59d62881573ddd
mahindraD <d.mahindra04010611@gmail.com>
2024-05-24 23:34:56 +0530

    Create and add OAuth component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState} from 'react'
 import { Link,useNavigate } from 'react-router-dom';
 import { useDispatch,useSelector} from 'react-redux';
 import { signInStart,signInSuccess,singInFailure } from '../redux/user/useSlice';
+import OAuth from '../components/OAuth';
 
 const SignIn = () => {
  
@@ -51,6 +52,7 @@ const SignIn = () => {
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
             { loading ? 'Loading..' :'sign in' }
         </button>
+        <OAuth />
       </form>
       <div className='flex gap-2 mt-5 mx-auto mx-auto'>
         <p>Dont have an account ?</p>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState} from 'react'
 import { Link,useNavigate } from 'react-router-dom';
-
+import OAuth from '../components/OAuth';
 
 const SignUp = () => {
 
@@ -57,6 +57,7 @@ const SignUp = () => {
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
             { loading ? 'Loading..' :'sign up' }
         </button>
+        <OAuth />
       </form>
       <div className='flex gap-2 mt-5 mx-auto mx-auto'>
         <p>Have an account ?</p>
